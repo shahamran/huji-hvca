516ba8685a89f8f2447688bfda08ae9b756b701c
Ran Shaham <shahamran91@gmail.com>
2017-05-03 02:28:49 +0300

    add all (fucking windows)

--- ex2/ex2.m
+++ ex2/ex2.m
@@ -20,7 +20,61 @@ v_flowers = Full_LK(I1, I2, lambda, flowersMask, num_iterations)
 % v_flowers = [-1.1675, -0.0010]
 
 %% question 6
+REAL_VELOCITY = 1;
+L2_DIFF = 2;
+
 squareSize = 128;
 sigmas = [10, 1];
+%lambda = 0.001;
+lambda = 0;
+mask = ones(squareSize);
+num_iterations = 1;
+maxVelocity = squareSize / 3;
+velocities = (-maxVelocity):maxVelocity;
+results = zeros(length(velocities), 2);
+
+hold on;
+for sigma = sigmas
+    firstFrame = GausSpot(squareSize, sigma, [0, 0]);
+    for i = 1:numel(velocities)
+        real_v = velocities(i);
+        secondFrame = GausSpot(squareSize, sigma, [real_v, 0]);
+        estimated_v = Full_LK(firstFrame, secondFrame, lambda, ...
+                              mask, num_iterations);
+        results(i, REAL_VELOCITY) = real_v;
+        % the L2 norm of a scalar is the abs value
+        results(i, L2_DIFF) = abs(estimated_v(1) - real_v);
+    end
+    plot(results(:, REAL_VELOCITY), results(:, L2_DIFF));
+end
+hold off;
+title({'LK estimation error on a gaussian spot', ...
+       ['\lambda = ', num2str(lambda)], ...
+       [' Square size = ', num2str(squareSize)]});
+xlabel('True Velocity');
+ylabel({'Estimation Error', '(L_2 norm of difference)'});
+legend(['\sigma = ', num2str(sigmas(1))], ...
+       ['\sigma = ', num2str(sigmas(2))]);
 
 %% question 7
+THIN = 0;
+FAT = 1;
+lambda = 0.01;
+contrasts = 1:-0.2:0;
+iters = [5, 25, 125];
+
+result = zeros(numel(contrasts), numel(iters), 2);
+for i = 1:numel(contrasts)
+    contrast = contrasts(i);
+    for k = [THIN, FAT]
+        [rhom1, rhom2] = rhombusMovie(k, contrast);
+        mask = ones(size(rhom1));
+        for j = 1:numel(iters)
+            iter = iters(j);
+            v = Full_LK(rhom1, rhom2, lambda, mask, iter);
+            results(i, j, k+1) = v;
+        end
+    end
+end
+plot(contrasts, results(:, 1, 1));
+figure;plot(contrasts, results(:, 1, 2));
